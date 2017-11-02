1d7fc2743d4857859afc4da9b24343d87935ae3b
kcharles52 <kcharles52@gmail.com>
2017-11-02 12:01:05 +0300

    inistial setup

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const stripe = require('stripe')('sk_test_2e4w1nQmLwRH4WlfVR3DFJS9');
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+
+const app = express();
+//handlebars Middleware
+app.engine('handlebars',exphbs({defaultLayout:'main'}));
+app.set('view engine','handlebars');
+
+//body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+//Static folder
+app.use(express.static('${__dirname}/public'));
+
+//Index Route
+app.get('/',(req,res)=>{
+    res.render('Index');
+});
+const port =process.env.PORT || 5000;
+
+app.listen(port,()=>{
+    console.log('Server started on port ${port}');
+});
