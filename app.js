b92536e1d4ead50b7115982a3a09175a26839269
kcharles52 <kcharles52@gmail.com>
2017-11-02 12:18:40 +0300

    changing the wrong quotes used - back quotes needed

--- app.js
+++ app.js
@@ -13,14 +13,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
 //Static folder
-app.use(express.static('${__dirname}/public'));
+app.use(express.static(`${__dirname}/public`));
 
 //Index Route
 app.get('/',(req,res)=>{
     res.render('Index');
 });
-const port =process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port,()=>{
-    console.log('Server started on port ${port}');
+    console.log(`Server started on port ${port}`);
 });
