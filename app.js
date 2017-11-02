50773dcf2f11ac3ee1542b52f4ad82d97180139d
kcharles52 <kcharles52@gmail.com>
2017-11-02 13:07:20 +0300

    charge route

--- app.js
+++ app.js
@@ -19,6 +19,24 @@ app.use(express.static(`${__dirname}/public`));
 app.get('/',(req,res)=>{
     res.render('Index');
 });
+
+//charge route
+app.post('/charge', (req,res)=>{
+    const amout = 2500;
+
+    stripe.customers.create({
+        email:req.body.stripeEmail,
+        source:req.body.stripeToken
+    })
+    .then(customer=>stripe.charges.create({
+        amount,
+        description:'Web Development Ebook',
+        currency:'usd',
+        customer:customer.id
+    }))
+    .then(charge => res.render('success'));
+
+});
 const port = process.env.PORT || 5000;
 
 app.listen(port,()=>{
